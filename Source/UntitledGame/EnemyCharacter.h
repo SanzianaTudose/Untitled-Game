589a78e584908a704c288528ff4f57f9ee19ad21
SanzianaTudose <tudosesanziana@gmail.com>
2023-10-11 20:33:41 +0200

    Set-up for Enemy classes
    + EnemyCharacter class and BP
    + EnemyAIController class and BP

---
+++ Source/UntitledGame/EnemyCharacter.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyCharacter.generated.h"
+
+UCLASS()
+class UNTITLEDGAME_API AEnemyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
