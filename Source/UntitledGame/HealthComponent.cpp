67fbe035c706936709c676d3985f1a7bde8f3de1
SanzianaTudose <tudosesanziana@gmail.com>
2023-10-14 16:31:38 +0200

    Damage System set-up
    + add HealthComponent class
    + add HealthComponent to EnemyCharacter

---
+++ Source/UntitledGame/HealthComponent.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Health = MaxHealth;
+
+	// Bind callback function (UHealthComponent::DamageTaken) to OnTakeAnyDamage delegate
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::DamageTaken);
+}
+
+
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UHealthComponent::DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* Instigator, AActor* DamageCauser)
+{
+
+}
+
+
