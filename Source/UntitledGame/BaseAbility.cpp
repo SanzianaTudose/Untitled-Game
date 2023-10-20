4fa428e84348eab1ee12dee2f66aacdec11df8b9
Willem Ploegstra <72265671+Willem-P1@users.noreply.github.com>
2023-10-20 17:59:25 +0200

    Ability system (#7)
    Story ID: 11
    
    AbilityTest map showcases two different ability types. (currently not implemented in Weapon Core)
    
    * Initial implementations for ability system
    
    Added AbilityObject and BaseAbility, and two temporary objects showing the modifications, addditionally already implemented the collision.
    
    * Data transfer  to ability object
    
    * Data transfer fix and movement
    
    * Spawn Abilityobject at right location
    
    * Issues
    
    * Build error fix
    
    * Ability Object rotation works correctly
    
    Simple way of fixing he issue by actually using the function correctly
    
    * Added some meshes to the ability objects

---
+++ Source/UntitledGame/BaseAbility.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseAbility.h"
+#include "AbilityObjectComponent.h"
+// Sets default values
+ABaseAbility::ABaseAbility()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseAbility::BeginPlay()
+{
+	Super::BeginPlay();
+	spawnObjects();
+	
+}
+
+// Called every frame
+void ABaseAbility::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseAbility::spawnObjects()
+{
+	UWorld* const World = GetWorld();
+
+	for(auto abilityClass : abilityObjects)
+	{
+		if (World != nullptr)
+		{
+			// spawn the projectile
+			//this does not work
+			AActor* actor = World->SpawnActor<AActor>(abilityClass, GetActorLocation(), GetActorRotation());
+			//this works
+			// AActor* actor = World->SpawnActor(abilityClass);
+			// actor->SetActorRotation(GetActorRotation());
+			// actor->SetActorLocation(GetActorLocation());
+			UAbilityObjectComponent* obj = actor->FindComponentByClass<UAbilityObjectComponent>();
+			if(obj)
+			{
+				obj->setData(damage, destroyOnHit, range);
+			}
+			UE_LOG(LogTemp, Warning, TEXT("Object Spawned"));
+		}
+	}
+}
+
+
+
